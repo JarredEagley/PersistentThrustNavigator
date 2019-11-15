58142ce20041761c9055a9f48fb7b8e65e5ca0fd
JarredEagley <jarred.eagley@gmail.com>
2019-11-14 18:41:42 -0800

    fixed MouseDown and MouseUp EventTypes

--- SolarSailNavigator/SolarSailNavigatorHotfix/Utils.cs
+++ SolarSailNavigator/SolarSailNavigatorHotfix/Utils.cs
@@ -73,13 +73,13 @@ namespace SolarSailNavigator {
 		int controlID = GUIUtility.GetControlID(popupListHash, FocusType.Passive);
 		bool done = false;
 		switch (Event.current.GetTypeForControl(controlID)) {
-		    case EventType.mouseDown:
+		    case EventType.MouseDown:
 			if (position.Contains(Event.current.mousePosition)) {
 			    GUIUtility.hotControl = controlID;
 			    showList = true;
 			}
 			break;
-		    case EventType.mouseUp:
+		    case EventType.MouseUp:
 			if (showList) {
 			    done = true;
 			}
